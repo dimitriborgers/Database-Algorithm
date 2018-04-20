38d235661910ad8da72644006bebc346f2fe8a31
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-04-19 22:39:28 -0400

    Reading files Draft 1

--- Read.java
+++ Read.java
@@ -1,14 +1,44 @@
 import java.io.File;
 import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
 
 public class Read {
 
     public static void main(String [] args) throws Exception {
+        //Query.txt
+        //Open file, create scanner, and ArrayList
         File query = new File("query.txt");
-        Scanner sc = new Scanner(query);
+        Scanner squery = new Scanner(query);
+        ArrayList<Float[]> lineArray = new ArrayList<Float[]>();
 
-        while (sc.hasNextLine()) {
-            System.out.println(sc.nextLine());
+        //Loop through each line, split by space, and create an array
+        while (squery.hasNextLine()) {
+            String[] stokens = squery.nextLine().split("\\s+");
+            Float[] arrSelec = new Float[stokens.length];
+            for (int i = 0; i < stokens.length; i++) {
+                float selec = Float.parseFloat(stokens[i]);
+                arrSelec[i] = selec;
+            }
+            //Insert array into global array list
+            lineArray.add(arrSelec);
         }
+        //Close file to prevent memory leak
+        squery.close();
+
+        //Config.txt
+        //Open file, create scanner, and Map
+        File config = new File("config.txt");
+        Scanner sconfig = new Scanner(config);
+        Map<String, Integer> configMap = new HashMap<String, Integer>();
+
+        //Loop through each line, split by equal sign, and add to Map
+        while (sconfig.hasNextLine()) {
+            String[] ctokens = sconfig.nextLine().split(" = ");
+            int cost = Integer.parseInt(ctokens[1]);
+            configMap.put(ctokens[0],cost);
+        }
+        //Close file tp provent memory leak
+        sconfig.close();
     }
 }
