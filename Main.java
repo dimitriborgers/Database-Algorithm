ca34f18f302461dcf2697a585c06301ff5270f9b
Gilad <freeslugs@gmail.com>
2018-04-20 19:02:54 -0400

    init calc 2, print statements

--- Main.java
+++ Main.java
@@ -8,13 +8,15 @@ public class Main {
         int power = (int) Math.pow(2,k);
         List<List<Double>> dict = new ArrayList<List<Double>>();
 
+        System.out.print("Possible plans: \n");
+
         for (int i = 1; i < power; i++) {
             List<Double> tempArray = new ArrayList<Double>();
 
             for (int j = 0; j < k; j++) {
                 if ((i & (1 << j)) > 0) {
                     tempArray.add(arr[j]);
-                    System.out.print(arr[j]);
+                    System.out.print(arr[j] + " ");
                 }
             }
             System.out.println("");
@@ -22,6 +24,15 @@ public class Main {
         }
         CalcOne(dict);
         return dict;
+    }
+
+    public static void CalcTwo(List<List<Double>> dict) throws Exception {
+        Read config = new Read();
+        Map<String, Double> configurations = config.cfetch();
+
+        double r = configurations.get("r"); double t = configurations.get("t");
+        double l = configurations.get("l"); double m = configurations.get("m");
+        double a = configurations.get("a"); double f = configurations.get("f");
     }
 
     public static void CalcOne(List<List<Double>> dict) throws Exception {
@@ -42,16 +53,18 @@ public class Main {
             for (double num : temp) {
                 p *= num;
             }
+
             double q;
             if (p <= .5) { q = p; } else { q = 1 - p; }
 
             double logicalAnd = k*r + ((k - 1)*l) + (k*f) + t + m*q + (p*a);
-            System.out.print(logicalAnd + " ");
+            System.out.print(temp + " => p: " + p + "\n");
+            System.out.print(" - logicalAnd: " + logicalAnd + " \n");
             double noBranch = k*r + ((k - 1)*l) + (k*f) + a;
-            System.out.print(noBranch + " ");
+            System.out.print(" - nobranch: " + noBranch + " \n");
 
             best[count]=Math.min(logicalAnd, noBranch);
-            System.out.println (best[count]);
+            System.out.println (" - best: " + best[count]);
             count++;
         }
     }
