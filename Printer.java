153f7452d861b1d414ebed0bc8151a38bd7ad048
Gilad <freeslugs@gmail.com>
2018-04-22 00:43:50 -0400

    update printer with better inputs, fix bugs to support lists and arrays

--- Printer.java
+++ Printer.java
@@ -8,31 +8,30 @@ public class Printer {
     }
 
     public static void print(List<Double> selectivities, Double[] bestPath, Double cost, Boolean logicalAnd) throws Exception {
+        System.out.println("==================================================================");
 
+        // Print the selectivities
+        String selectivitiesStr = "";
+        for (Double s : selectivities) {
+            selectivitiesStr += (s + " ");
+        }
+        System.out.println(selectivitiesStr);
 
-        // System.out.println("==================================================================");
+        System.out.println("------------------------------------------------------------------");
 
-        // // Print the selectivities
-        // String[] selectivitiesArr = Arrays.stream(selectivities).map(s -> s.toString()).toArray(String[]::new);
-        // String selectivitiesStr = String.join(" ", selectivitiesArr);
-        // System.out.println(selectivitiesStr);
+        // print out if and terms 
+        String[] terms = new String[bestPath.length];
+        for (int i = 0; i < bestPath.length; i++) {
+            int index = selectivities.indexOf(bestPath[i]);
+            terms[i] = formatExp(index+1);
+        }
+        String ifStr = "if(" + String.join(" && ", terms) + ") {";
+        System.out.println(ifStr);
 
-        // System.out.println("------------------------------------------------------------------");
+        System.out.println("    answer[j++] = i;");
+        System.out.println("}");
 
-        // String[] terms = new String[best.length];
-
-        // for (int i = 0; i < best.length; i++) {
-        //     // int index = selectivities.indexOf(best[i]);
-        //     int index = java.util.Arrays.asList(selectivities).indexOf(best[i]);
-        //     terms[i] = formatExp(index+1);
-        // }
-        // String ifStr = "if(" + String.join(" && ", terms) + ") {";
-        // System.out.println(ifStr);
-
-        // System.out.println("    answer[j++] = i;");
-        // System.out.println("}");
-
-        // System.out.println("------------------------------------------------------------------");
-        // System.out.println("cost: " + cost);
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("cost: " + cost);
     }
 }
