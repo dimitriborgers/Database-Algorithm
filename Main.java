6f340314ae6bb74788156058583e639bca59af1b
Gilad <freeslugs@gmail.com>
2018-04-22 20:54:02 -0400

    remove vestigial print statments'

--- Main.java
+++ Main.java
@@ -54,8 +54,7 @@ public class Main {
         double r = configurations.get("r"); double t = configurations.get("t");
         double l = configurations.get("l"); double m = configurations.get("m");
         double a = configurations.get("a"); double f = configurations.get("f");
-        // System.out.println("start: " + input);
-
+        
         //checks to see if noBranching And has been used
         boolean noUsed = false;
 
@@ -66,14 +65,12 @@ public class Main {
                 Double temp = input.get(i);
                 arr.add(temp);
             }
-            // System.out.println("input: " + input + "   remainder " + arr);
-
+            
             Double tempCost = Recurse(arr);
-            // System.out.println("temporary costs: " + tempCost);
-
+            
             double p = 1;
             double k = input.size();
-            // System.out.println("input size:   " + k);
+            
             for (double num : input) {
                 p *= num;
             }
@@ -81,16 +78,14 @@ public class Main {
             if (p <= .5) { q = p; } else { q = 1 - p; }
 
             Double logicalAnd2 = k*r + ((k - 1)*l) + (k*f) + t + m*q + (p*a);
-            // System.out.println("logicalAnd:  " + logicalAnd2);
             Double branchingAnd = fcost(input.get(0)) + (m*q) + p*(tempCost);
-            // System.out.println("branchingAnd:    " + branchingAnd);
+            
             if (branchingAnd < logicalAnd2) {
                 noUsed = true;
             }
             //check to see if noBranch has been used. Once it's been used once, it can't be used again.
             if (noUsed == false) {
                 Double noBranch = k*r + ((k - 1)*l) + (k*f) + a;
-                // System.out.println("no branch:   " + noBranch);
                 Double bestOf1 = Math.min(logicalAnd2,branchingAnd);
                 if (logicalAnd2 == bestOf1) {
                     logicalAndBool = true;
@@ -102,7 +97,6 @@ public class Main {
                 return bestOf2;
             } else {
                 Double bestOfTwo2 = Math.min(logicalAnd2,branchingAnd);
-                // System.out.println("BestCost:   " + bestOfTwo2);
                 return bestOfTwo2;
             }
         } else {
@@ -114,7 +108,6 @@ public class Main {
             Double logicalAnd = k*r + ((k - 1)*l) + (k*f) + t + m*q + (p*a);
             Double noBranch = k*r + ((k - 1)*l) + (k*f) + a;
             Double bestOfTwo = Math.min(logicalAnd,noBranch);
-            // System.out.println("Else costs: " + bestOfTwo);
             return bestOfTwo;
         }
     }
@@ -136,14 +129,9 @@ public class Main {
         Double bestPath = Double.POSITIVE_INFINITY;
         // for each combination use recurse to find the min for a path.
         for (Double[] d : combinations) {
-            // test print the combinations
-            for (Double t : d) {
-                System.out.print(t + ", ");
-            }
             List<Double> list = Arrays.asList(d);
             Double cost = Recurse(list);
-            //System.out.println(" => " + cost);
-
+            
             if(cost < bestPath) {
                 bestPath = cost;
                 BestCombo = d;
